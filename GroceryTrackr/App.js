e4ca531b6ed2f9f781cdf028ef5ecae1e4915fd4
Bradley Pregon <bradley.pregon@gmail.com>
2022-04-21 14:07:57 -0500

    adding support for dark mode

--- GroceryTrackr/App.js
+++ GroceryTrackr/App.js
@@ -1,38 +1,52 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Appearance } from "react-native";
+import { useState } from "react";
 
-import TripsScreen from './Screens/TripsScreen';
-import TripDetailScreen from './Screens/TripDetailScreen';
+import {
+	NavigationContainer,
+	DefaultTheme,
+	DarkTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import NewTripModal from './Screens/NewTripModal';
+import TripsScreen from "./Screens/TripsScreen";
+import TripDetailScreen from "./Screens/TripDetailScreen";
 
-const Stack = createNativeStackNavigator();
+import NewTripModal from "./Screens/NewTripModal";
 
-export default function App({navigation}) {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'TripsScreen'}>
-        <Stack.Group>
-          <Stack.Screen 
-            name="TripsScreen"
-            component={TripsScreen}
-            options={{
-              title: 'Trips', 
-              headerLargeTitle: true
-            }}
-          />
-          <Stack.Screen
-            name="TripDetailScreen"
-            component={TripDetailScreen}
-            options={({route}) => ({title: route.params.name, headerLargeTitle: true})}
-          />
-        </Stack.Group>
-        
-        <Stack.Group screenOptions={{ presentation: 'modal'}}>
-          <Stack.Screen name="NewTripModal" component={NewTripModal} />
-        </Stack.Group>
-        
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default function App({ navigation }) {
+	const Stack = createNativeStackNavigator();
+
+	const [theme, setTheme] = useState(Appearance.getColorScheme());
+	Appearance.addChangeListener((scheme) => {
+		setTheme(scheme.colorScheme);
+	});
+
+	return (
+		<NavigationContainer theme={theme == "dark" ? DarkTheme : DefaultTheme}>
+			<Stack.Navigator initialRouteName={"TripsScreen"}>
+				<Stack.Group>
+					<Stack.Screen
+						name="TripsScreen"
+						component={TripsScreen}
+						options={{
+							title: "Trips",
+							headerLargeTitle: true,
+						}}
+					/>
+					<Stack.Screen
+						name="TripDetailScreen"
+						component={TripDetailScreen}
+						options={({ route }) => ({
+							title: route.params.name,
+							headerLargeTitle: true,
+						})}
+					/>
+				</Stack.Group>
+
+				<Stack.Group screenOptions={{ presentation: "modal" }}>
+					<Stack.Screen name="NewTripModal" component={NewTripModal} />
+				</Stack.Group>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
