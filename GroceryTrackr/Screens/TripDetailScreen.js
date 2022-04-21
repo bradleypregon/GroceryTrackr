b35940d75663eb2313ebe2b8c51cba54867db920
Bradley Pregon <bradley.pregon@gmail.com>
2022-04-21 14:07:47 -0500

    added react native elements package

--- GroceryTrackr/Screens/TripDetailScreen.js
+++ GroceryTrackr/Screens/TripDetailScreen.js
@@ -1,62 +1,63 @@
-import { StyleSheet, View, FlatList } from 'react-native';
-import uuid from 'react-native-uuid';
-
-import TripDetailItem from './TripDetailItem';
+import { FlatList } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ListItem } from "@rneui/themed";
+import TouchableScale from "react-native-touchable-scale";
 
 // dummy data
 var testItems = [
-{
-  brand: "Frito Lay",
-  item: "Doritos",
-  key: uuid.v4(),
-  price: 2.5,
-  quantity: 1,
-  subtotal: 2.5,
-},
-{ 
-  brand: "Pepsi",
-  item: "Mountain Dew",
-  key: uuid.v4(),
-  price: 2.5,
-  quantity: 2,
-  subtotal: 5.00,
-},
-]
+	{
+		brand: "Frito Lay",
+		item: "Doritos",
+		price: 2.5,
+		quantity: 1,
+		subtotal: 2.5,
+	},
+	{
+		brand: "Pepsi",
+		item: "Mountain Dew",
+		price: 2.5,
+		quantity: 2,
+		subtotal: 5.0,
+	},
+];
 
 const TripDetailScreen = (item) => {
-  return(
-    <View style={styles.container}>
+	const keyExtractor = (item, index) => index.toString();
 
-      <FlatList
-        data={testItems}
-        renderItem={({item}) => {
-          return (
-            <TripDetailItem
-              brand={item.brand}
-              item={item.item}
-              price={item.price}
-              quantity={item.quantity}
-            />
-          );
-        }}
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.listStyle}
-      />
+	const renderItem = ({ item }) => (
+		<ListItem
+			Component={TouchableScale}
+			friction={20}
+			tension={100}
+			activeScale={0.9}
+			containerStyle={{
+				width: "90%",
+				borderRadius: 5,
+				overflow: "hidden",
+				borderWidth: 0.5,
+				borderColor: "gray",
+				margin: 3,
+				justifyContent: "center",
+				alignSelf: "center",
+			}}
+		>
+			<ListItem.Content>
+				<ListItem.Title>{item.item}</ListItem.Title>
+				<ListItem.Subtitle>{item.brand}</ListItem.Subtitle>
+			</ListItem.Content>
+		</ListItem>
+	);
 
-    </View>
-  );
+	return (
+		<SafeAreaProvider>
+			<FlatList
+				keyExtractor={keyExtractor}
+				data={testItems}
+				renderItem={renderItem}
+				contentInsetAdjustmentBehavior="automatic"
+			/>
+		</SafeAreaProvider>
+	);
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  listStyle: {
-    width: '90%', 
-    padding: 10
-  },
-});
 
 export default TripDetailScreen;
--- GroceryTrackr/Screens/TripsScreen.js
+++ GroceryTrackr/Screens/TripsScreen.js
@@ -1,80 +1,138 @@
-import { FlatList, View, StyleSheet, TouchableOpacity, Button } from 'react-native'
-import TripItem from './TripItem';
-import uuid from 'react-native-uuid';
-import React from 'react';
+import { FlatList, Button } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ListItem } from "@rneui/themed";
+import TouchableScale from "react-native-touchable-scale";
+
+import React from "react";
 
 // Dummy Data
 var testData = [
-  {
-  store: "HyVee",
-  location: "fairfield",
-  key: uuid.v4(),
-  items: ["item 1", "item 2"],
-  date: Date()
-},
-{
-  store: "WalMart",
-  location: "fairfield",
-  key: uuid.v4(),
-  items: ["item 1", "item 2"],
-  date: Date()
-},
-{
-  store: "Aldi's",
-  location: "Ottumwa",
-  key: uuid.v4(),
-  items: ["item 1", "item 2"],
-  date: Date()
-}]
+	{
+		store: "HyVee",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Wal-Mart",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Aldi's",
+		location: "Ottumwa",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "HyVee",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Wal-Mart",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Aldi's",
+		location: "Ottumwa",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "HyVee",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Wal-Mart",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Aldi's",
+		location: "Ottumwa",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "HyVee",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Wal-Mart",
+		location: "Fairfield, IA",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+	{
+		store: "Aldi's",
+		location: "Ottumwa",
+		items: ["item 1", "item 2"],
+		date: Date(),
+	},
+];
 
-function TripsScreen({navigation}) {
+function TripsScreen({ navigation }) {
+	React.useLayoutEffect(() => {
+		navigation.setOptions({
+			headerRight: () => (
+				<Button
+					onPress={() => navigation.navigate("NewTripModal")}
+					title="New"
+				/>
+			),
+		});
+	});
 
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button
-          onPress={() => navigation.navigate('NewTripModal')}
-          title="New"
-        />
-    )
-    })
-  })
-  
+	const keyExtractor = (item, index) => index.toString();
+	const renderItem = ({ item }) => (
+		<ListItem
+			Component={TouchableScale}
+			friction={20}
+			tension={100}
+			activeScale={0.9}
+			containerStyle={{
+				width: "90%",
+				borderRadius: 5,
+				overflow: "hidden",
+				borderWidth: 0.5,
+				borderColor: "gray",
+				margin: 3,
+				justifyContent: "center",
+				alignSelf: "center",
+			}}
+			onPress={() =>
+				navigation.navigate("TripDetailScreen", { name: item.store })
+			}
+		>
+			<ListItem.Content>
+				<ListItem.Title>{item.store}</ListItem.Title>
+				<ListItem.Subtitle>
+					{item.location} - {item.date}
+				</ListItem.Subtitle>
+			</ListItem.Content>
+		</ListItem>
+	);
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={testData}
-        renderItem={({item}) => {
-          return(
-            <TouchableOpacity onPress={() => navigation.navigate('TripDetailScreen', {name: item.store} )}>
-              <TripItem
-                store={item.store}
-                location={item.location}
-                date={item.date}
-              />
-            </TouchableOpacity>
-          )
-        }}
-        
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.listStyle}
-      />
-
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center',
-  },
-  listStyle: {
-    width: '90%', 
-    padding: 10
-  }
-});
+	return (
+		<SafeAreaProvider>
+			<FlatList
+				keyExtractor={keyExtractor}
+				data={testData}
+				renderItem={renderItem}
+				contentInsetAdjustmentBehavior="automatic"
+			/>
+		</SafeAreaProvider>
+	);
+}
 
 export default TripsScreen;
